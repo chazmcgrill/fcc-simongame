3e83f6c5925ef81c935cc414a28872b4eed32193
chazmcgrill <charlietaylordev@gmail.com>
2017-07-28 18:53:47 +0100

    Setup Event Listeners and Basic DOM Manipulation
    Added click event listeners for each of the buttons. Colour changes on
    middle buttons and slide animation for on/off switch.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
   console.log('site loaded');
+  var startBtn = false,
+      strictBtn = false;
+
+  // on/off switch click event
+  $('.onoff-btn').click(function() {
+    var $this = $(this);
+    if ($this.css('left') === '20px') {
+      $this.css('left', '2px');
+    } else {
+      $this.css('left', '20px');
+    }
+  });
+
+  $('.start-btn').click(function() {
+    if (startBtn) {
+      $(this).css('background', '#4FB0C6');
+      startBtn = false;
+    } else {
+      $(this).css('background', '#9eedff');
+      startBtn = true;
+    }
+  });
+
+  $('.strict-btn').click(function() {
+    if (strictBtn) {
+      $(this).css('background', '#6C49B8');
+      strictBtn = false;
+    } else {
+      $(this).css('background', '#c9b3f9');
+      strictBtn = true;
+    }
+  });
+
+
 });
