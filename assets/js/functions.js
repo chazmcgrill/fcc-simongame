d2aaf02c5128bf1b576d749723a1ba11fd4ae629
chazmcgrill <charlietaylordev@gmail.com>
2017-07-29 16:30:45 +0100

    Fixed Display Function / Click Events
    Fixed display function by using recursion. Added click events for the
    main buttons. Amended syntax for buttons as still using primary colours
    as class names.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,26 +4,51 @@ $(document).ready(function(){
       strictBtn = false,
       // array that contains pattern
       pattern = [];
+      // current = 0;
 
   // create random pattern
   function patGen() {
     var peice = Math.floor(Math.random() * 4) + 1;
     pattern.push(peice);
     console.log(pattern);
-    displayPattern(peice);
+    displayPattern(pattern, 0, pattern.length - 1);
   }
-
-  console.log(patGen());
 
   // display pattern
-  function displayPattern(peice) {
-    $('#' + peice).addClass('filter');
-    // play specified sound
-    setTimeout(function(){
-      // add highlight to css
-      $('#' + peice).removeClass('filter');
-    }, 500);
+  function displayPattern(array, count, length) {
+    if(count === length) {
+      console.log(array[count] + ' = final count');
+      $('#' + array[count]).addClass('filter');
+      // play specified sound
+      setTimeout(function(){
+        $('#' + array[count]).removeClass('filter');
+      }, 1000);
+      return 0;
+    } else {
+      console.log(array[count]);
+      $('#' + array[count]).addClass('filter');
+      // play specified sound
+      setTimeout(function(){
+        // add highlight to css
+        $('#' + array[count]).removeClass('filter');
+        return displayPattern(array, count + 1, length);
+      }, 1000);
+    }
   }
+
+  // button click events
+  $('.buttons').each(function() {
+    $(this).click(function() {
+      console.log($(this).attr('id'));
+      console.log(pattern[current]);
+      if ($(this).attr('id') == pattern[current]) {
+        console.log('correct');
+      } else {
+        console.log('incorrect');
+      }
+
+    });
+  });
 
   // check if correct button pressed
 
@@ -36,7 +61,7 @@ $(document).ready(function(){
   // strict mode
 
 
-
+  // sound files
 
 
   // on/off switch click event
@@ -57,6 +82,7 @@ $(document).ready(function(){
     } else {
       $(this).css('background', '#9eedff');
       startBtn = true;
+      patGen();
     }
   });
 
