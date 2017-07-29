e9e5d0b0b12dcb1e80df78222a71f284a0a419bc
chazmcgrill <charlietaylordev@gmail.com>
2017-07-29 14:18:53 +0100

    JS Added Functionality Move Selector Show Pattern Selector
    JS Added Functionality Move Selector Show Pattern Selector

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,43 @@
 $(document).ready(function(){
   console.log('site loaded');
   var startBtn = false,
-      strictBtn = false;
+      strictBtn = false,
+      // array that contains pattern
+      pattern = [];
+
+  // create random pattern
+  function patGen() {
+    var peice = Math.floor(Math.random() * 4) + 1;
+    pattern.push(peice);
+    console.log(pattern);
+    displayPattern(peice);
+  }
+
+  console.log(patGen());
+
+  // display pattern
+  function displayPattern(peice) {
+    $('#' + peice).addClass('filter');
+    // play specified sound
+    setTimeout(function(){
+      // add highlight to css
+      $('#' + peice).removeClass('filter');
+    }, 500);
+  }
+
+  // check if correct button pressed
+
+  // show error if incorrect button pressed
+
+  // continue pattern if correct button pressed
+
+  // counter increase for each new pattern
+
+  // strict mode
+
+
+
+
 
   // on/off switch click event
   $('.onoff-btn').click(function() {
@@ -13,6 +49,7 @@ $(document).ready(function(){
     }
   });
 
+  // start button click event
   $('.start-btn').click(function() {
     if (startBtn) {
       $(this).css('background', '#4FB0C6');
@@ -23,6 +60,7 @@ $(document).ready(function(){
     }
   });
 
+  // strict button click event
   $('.strict-btn').click(function() {
     if (strictBtn) {
       $(this).css('background', '#6C49B8');
