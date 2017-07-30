c522d19b59fef6d4a860a5ad14851e6abe90c27c
chazmcgrill <charlietaylordev@gmail.com>
2017-07-30 10:12:07 +0100

    On / Off Function
    Added on off functionality and created button locks for off status.
    Changed start button so it restarts if already in an existing game.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,9 +2,10 @@ $(document).ready(function(){
   console.log('site loaded');
   var startBtn = false,
       strictBtn = false,
+      gameOn = false,
       pattern = [],
       current = 0,
-      level = 0;
+      level = 1;
 
   // create random pattern
   function patGen() {
@@ -34,39 +35,42 @@ $(document).ready(function(){
   // button click events
   $('.buttons').each(function() {
     $(this).click(function() {
-      console.log($(this).attr('id'));
-      console.log(pattern[current]);
+      if (gameOn) {
+        console.log($(this).attr('id'));
+        console.log(pattern[current]);
 
-      // check if correct button pressed
-      if ($(this).attr('id') == pattern[current]) {
-        console.log('correct');
-        
-        // continue pattern if correct pattern pressed
-        if (current === pattern.length - 1) {
-          level++;
-          levelCounter(level);
-          current = 0;
-          patGen();
-        } else {
-          current++;
-        }
+        // check if correct button pressed
+        if ($(this).attr('id') == pattern[current]) {
+          console.log('correct');
 
-      // show error if incorrect button pressed
-      } else {
-        console.log('incorrect');
-        $('.count-screen').text('!!');
-        setTimeout(function() {
-          if (!strictBtn) {
+          // continue pattern if correct pattern pressed
+          if (current === pattern.length - 1) {
+            level++;
             levelCounter(level);
             current = 0;
-            displayPattern(pattern, 0, pattern.length - 1);
-
-          // strict mode
+            patGen();
           } else {
-            // reset
-            reset();
+            current++;
           }
-        }, 1000);
+
+          // show error if incorrect button pressed
+        } else {
+          console.log('incorrect');
+          $('.count-screen').text('!!');
+          setTimeout(function() {
+            if (!strictBtn) {
+              levelCounter(level);
+              current = 0;
+              displayPattern(pattern, 0, pattern.length - 1);
+
+              // strict mode
+            } else {
+              // reset
+              reset();
+            }
+          }, 1000);
+        }
+
       }
     });
   });
@@ -82,49 +86,58 @@ $(document).ready(function(){
   // reset function
   function reset() {
     levelCounter(0);
-    level = 0;
+    level = 1;
     pattern = [];
     $('.start-btn').css('background', '#4FB0C6');
     startBtn = !startBtn;
   }
 
-  // on / off switch
-
   // light up button and play sound when pressed
 
   // sound files
 
-  // lock click during pattern display
+  // space between button lights (css animation?)
 
+  // TRANSFER THIS TO CSS FOR MEDIA QUERY
   // on/off switch click event
   $('.onoff-btn').click(function() {
     var $this = $(this);
-    if ($this.css('left') === '20px') {
+    if (!gameOn) {
       $this.css('left', '2px');
     } else {
       $this.css('left', '20px');
+      reset();
     }
+    gameOn = !gameOn;
   });
 
+
+  // THESE CAN BE MERGED
   // start button click event
   $('.start-btn').click(function() {
-    if (startBtn) {
-      $(this).css('background', '#4FB0C6');
-    } else {
-      $(this).css('background', '#9eedff');
-      patGen();
+    if (gameOn) {
+      if (!startBtn) {
+        $(this).css('background', '#9eedff');
+        patGen();
+      } else {
+        reset();
+        // screen blink
+        patGen();
+      }
+      startBtn = !startBtn;
     }
-    startBtn = !startBtn;
   });
 
   // strict button click event
   $('.strict-btn').click(function() {
-    if (strictBtn) {
-      $(this).css('background', '#6C49B8');
-    } else {
-      $(this).css('background', '#c9b3f9');
+    if (gameOn) {
+      if (strictBtn) {
+        $(this).css('background', '#6C49B8');
+      } else {
+        $(this).css('background', '#c9b3f9');
+      }
+      strictBtn = !strictBtn;
     }
-    strictBtn = !strictBtn;
   });
 
 
